c07ce5d8c586a763f0b250761cb07ad98dd929fc
Urbanz91 <turbino4ka91@gmail.com>
2019-01-08 14:55:32 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# instagram-skillbox
+This is app like instagram, based on unsplash API. You can view news feed, open photos and like that.
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+var miniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: 'development',
+  entry: './src',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'build.js'
+  },
+
+  plugins: [
+    new miniCssExtractPlugin({
+      filename: "build.css",
+      path: path.resolve(__dirname, 'build')
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: miniCssExtractPlugin.loader
+          },
+          'css-loader'
+        ]
+      }
+    ]
+  }
+}
