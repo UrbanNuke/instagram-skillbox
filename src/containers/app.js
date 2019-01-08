dae8970f29cdfbe7ebe31f79fc8bbc37d7a1b202
Urbanz91 <turbino4ka91@gmail.com>
2019-01-08 15:23:05 +0300

    prepare platform to start coding, create initial files

---
+++ src/containers/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+let App = (props) => {
+  return (
+    <div>
+      <h1>App works!</h1>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+
+import App from './containers/app';
+import {reducer} from './reducers/reducer';
+
+import {createStore} from 'redux';
+
+const initialState = [];
+
+// const store = createStore(reducer, initialState);
+
+ReactDOM.render(
+    <App/>,
+  document.querySelector('#app')
+);
