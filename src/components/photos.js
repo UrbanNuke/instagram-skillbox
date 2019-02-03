30514602481f795e1d37ea0fee01d778adbbd4a0
Urbanz91 <turbino4ka91@gmail.com>
2019-02-03 21:46:48 +0300

    ADD: lazyLoad for photos with scroll

--- src/components/photos.js
+++ src/components/photos.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import Photo from './photo';
 
-const Photos = ({photos, likePhoto, unlikePhoto}) => {
+const Photos = ({photos, likePhoto, unlikePhoto, addCurrentPhoto}) => {
+
+  
+
   return (
-    <section id="photos">
+    <section id="photos" onScroll={() => lazyLoadingScrollPhoto()}>
       <div className="col-md-12">
         <div className="photos-block">
           {
             photos.map((photo, index) => {
               return (
-                <Photo 
+                <Photo
                   photo={photo}
                   key={index}
                   likePhoto={likePhoto.bind(this)}
                   unlikePhoto={unlikePhoto.bind(this)}
+                  addCurrentPhoto={addCurrentPhoto.bind(this)}
                 />
               )
             })
